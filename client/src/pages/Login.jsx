1a2e48ff25dfb2c6d5aeb4f3a79a5841921190cb
Aung Myat Moe <aungmyatmoe834@gmail.com>
2023-01-12 22:28:22 +0630

    feat: add page title in login and register page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,107 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Alert, Button, Checkbox, Label, TextInput} from "flowbite-react";
+import http from "../../services/axios";
+import {Link, useNavigate} from "react-router-dom";
+
+function HiInformationCircle(prop) {
+    return <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+        <path fillRule="evenodd"
+              d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z"
+              clipRule="evenodd"
+              {...prop}
+        />
+    </svg>
+
+}
 
 const Login = () => {
+
+    const navigate = useNavigate();
+    const [inputs, setInputs] = useState({
+        email: "",
+        password: "",
+    });
+    const [error, setError] = useState(null);
+
+
+    const handleInputChange = (e) => {
+        setInputs((prev) => ({...prev, [e.target.name]: e.target.value}));
+    };
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            await http.post("auth/register", inputs)
+            navigate("/login")
+        } catch (error) {
+            setError(error.response.data.msg)
+        }
+    };
+
+
     return (
-        <div>
-            Login
+        <div className="flex flex-col justify-center items-center h-screen bg-gray-200">
+
+            <form className="flex flex-col gap-4 bg-gray-50 p-10  rounded-xl shadow" onSubmit={onSubmit} method="POST">
+
+                {
+                    error && <Alert
+                        color="failure"
+                        icon={HiInformationCircle}
+                        className="max-w-sm"
+                    >
+                        <p>
+                            <span className="font-medium">Info alert!</span>
+                            {' '}{error && error}
+                        </p>
+                    </Alert>
+                }
+
+                <h1 className="mb-3 text-center text-2xl font-semibold text-blue-600">Login</h1>
+
+                <div>
+                    <div className="mb-2 block">
+                        <Label
+                            htmlFor="email2"
+                            value="Your email"
+                        />
+                    </div>
+                    <TextInput
+                        id="email2"
+                        type="email"
+                        name="email"
+                        required={true}
+                        shadow={true}
+                        onChange={handleInputChange}
+                    />
+                </div>
+                <div>
+                    <div className="mb-2 block">
+                        <Label
+                            htmlFor="password2"
+                            value="Your password"
+                        />
+                    </div>
+                    <TextInput
+                        id="password2"
+                        type="password"
+                        required={true}
+                        shadow={true}
+                        name="password"
+                        onChange={handleInputChange}
+
+                    />
+                </div>
+                <Button type="submit">
+                    Login
+                </Button>
+                <div className="flex justify-center">
+                    <Link to="/register" className="text-sm font-medium text-blue-700 hover:text-blue-800">Don't you
+                        have an
+                        account yet?</Link>
+                </div>
+            </form>
         </div>
     );
 };
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,13 +1,13 @@
 import React, {useState} from 'react';
 import {Alert, Button, Checkbox, Label, TextInput} from "flowbite-react";
 import http from "../../services/axios";
-import {useNavigate} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 
 function HiInformationCircle(prop) {
     return <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-        <path fill-rule="evenodd"
+        <path fillRule="evenodd"
               d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z"
-              clip-rule="evenodd"
+              clipRule="evenodd"
               {...prop}
         />
     </svg>
@@ -33,8 +33,8 @@ const Register = () => {
         e.preventDefault();
 
         try {
-            const res = await http.post("auth/register", inputs)
-            navigate("/")
+            await http.post("auth/register", inputs)
+            navigate("/login")
         } catch (error) {
             setError(error.response.data.msg)
         }
@@ -45,6 +45,8 @@ const Register = () => {
         <div className="flex flex-col justify-center items-center h-screen bg-gray-200">
 
             <form className="flex flex-col gap-4 bg-gray-50 p-10  rounded-xl shadow" onSubmit={onSubmit} method="POST">
+
+                <h1 className="mb-3 text-center text-2xl font-semibold text-blue-600">Register</h1>
 
                 {
                     error && <Alert
@@ -121,9 +123,14 @@ const Register = () => {
                         </a>
                     </Label>
                 </div>
-                <Button type="submit">
+                <Button type="submit" className="mt-2">
                     Register new account
                 </Button>
+                <div className="mt-1 flex justify-center">
+                    <Link to="/login" className="text-sm font-medium text-blue-700 hover:text-blue-800">Already
+                        registered?
+                        Login</Link>
+                </div>
             </form>
         </div>
     );
